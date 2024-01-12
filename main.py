aa02a57bad4a4dba0b9db2208ad680e058ba59db
Alexis <56489898+Galaktik-hub@users.noreply.github.com>
2024-01-12 10:07:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,137 +1,186 @@
 #!/usr/bin/python3
 '''
-This program lets you create freely a README file for your GitHub profile or your projects.
+Recreation of the world-famous game of Minesweeper
 
-Throught a graphical interface, you can type in your text and the way you want to formate it.
+I know this program is not much of a revolution but I created to learn about GUI such as tkinter
 
-Once you're done creating your README file, you can press the 'Finished!' button and it will create the file in your current directory.
-
-In the commentary of the README, there will always be a credit to this program, I do not restreint you to remove it, though crediting someone for their work is always good!
+Please enjoy!
 '''
 ##########################################
 #           IMPORTING SECTION            #
 ##########################################
-#import PySimpleGUI as sg
-#import pygame as pg
+
 import tkinter as tk
 from random import randrange
+
 ##########################################
 #               GLOBAL VAR               #
 ##########################################
+
 BOARD_INFO = {
     "easy": (10, 15),
-    "medium": (15, 25),
+    "medium": (15, 35),
     "hard": (20, 35),
     "nightmare": (30, 45)
 }
 
-BOMB_BOARD = None
-USER_BOARD = None
-
 DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-GAME_OVER = False
 ##########################################
 
 #TODO: Create a way to mark a tile as "NOT SAFE" or something like that
 #TODO: Implement it in a graphical interface
+#TODO: Make the first click impossible to get a bomb, then in a given radius, have no bomb (Kind of like the google one)
+
+##########################################
+
+class Minesweeper:
+
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+        self.user_board = self.init_board()     # Board used to know where the bombs are, and display elements once the user revealed them
+        self.bomb_board = self.init_board()     # Board used to display the game
+        self.game_over_value = False
+        self.window = tk.Tk()
 
 
-def init_board(size):
-    '''Returns an empty square matrix of the size given in argument'''
-    return [['■']*size for _ in range(size)]
+    def init_board(self):
+        '''Returns an empty square matrix of the difficulty size'''
+        return [['■']*BOARD_INFO[self.difficulty][0] for _ in range(BOARD_INFO[self.difficulty][0])]
 
-
-def display_board(board):
-    '''Print in the console the board given in argument'''
-    print()
-    for line in board:
-        print('|', end=' ')
-
-        for tile in line:
-            print(tile, end = ' | ')
-
-        print()
-    print()
-
-
-def bombify(board, difficulty):
-    '''Function that randomly display bomb on the board given in argument, following a certain difficulty'''
-    nb_bombs = BOARD_INFO[difficulty][1]
     
-    while nb_bombs != 0:
+    def init_window(self):
+        '''Function that displays the game window'''
+        self.window.title("Minesweeper")
+        for i in range(BOARD_INFO[self.difficulty][0]):  # Creation of all the buttons that represent tiles
+            for j in range(BOARD_INFO[self.difficulty][0]):
+                self.window.columnconfigure(i, weight=1)
+                self.window.rowconfigure(j, weight=1)
 
-        bomb = (randrange(len(board)), randrange(len(board)))
+                frame = tk.Frame(
+                    master=self.window,
+                    relief=tk.RAISED,
+                    borderwidth=1
+                )
+                frame.grid(row=j, column=i)
+                label = tk.Button(master=frame, text=f"{i, j}")
+                label.pack()
 
-        if board[bomb[0]][bomb[1]] != 'X':
-            board[bomb[0]][bomb[1]] = 'X'
-            nb_bombs -= 1
-
-
-def near_bomb(board):
-    '''Function that modify the board given in argument to display the number of bomb within a 1 tile range'''
-    for i in range(len(board)):
-        for j in range(len(board)):
-
-            count = 0
-            if board[i][j] == 'X':
-                continue
-            for direction in DIRECTIONS:
-                if i + direction[0] >= 0 and i + direction[0] < len(board) and j + direction[1] >= 0 and j + direction[1] < len(board):
-                    if board[i + direction[0]][j + direction[1]] == 'X':
-                        count += 1
-            if count > 0:
-                board[i][j] = count
+        self.window.mainloop()
 
 
-def refresh_board(choice, visited = set()):
-    '''Function that refreshes the board for the user, showing blank spaces and number of bomb near tile'''
-    global USER_BOARD, BOMB_BOARD
+    def display_board(self, board):
+        '''Print in the console the board given in argument'''
+        print()
+        for line in board:
+            print('|', end=' ')
+
+            for tile in line:
+                print(tile, end = ' | ')
+
+            print()
+        print()
 
 
-    if choice not in visited:
-        if BOMB_BOARD[choice[0]][choice[1]] == '■':
-            USER_BOARD[choice[0]][choice[1]] = ' '
-            for direction in DIRECTIONS:
-                if choice[0] + direction[0] >= 0 and choice[0] + direction[0] < len(USER_BOARD) and choice[1] + direction[1] >= 0 and choice[1] + direction[1] < len(USER_BOARD):
-                    visited.add(choice)
-                    refresh_board((choice[0] + direction[0], choice[1] + direction[1]))
-        else:
-            USER_BOARD[choice[0]][choice[1]] = BOMB_BOARD[choice[0]][choice[1]]
+    def bombify(self):
+        '''Function that randomly display bomb on the bomb board, following a certain difficulty'''
+        nb_bombs = BOARD_INFO[self.difficulty][1]
+        
+        while nb_bombs != 0:    # To avoid having two bombs having the same coordinate, thus letting us with less bombs wanted
+            bomb = (randrange(len(self.bomb_board)), randrange(len(self.bomb_board)))
+
+            if self.bomb_board[bomb[0]][bomb[1]] != 'X':    # If the tile is already a bomb, we don't touch it
+                self.bomb_board[bomb[0]][bomb[1]] = 'X'
+                nb_bombs -= 1
 
 
-def play(board):
-    '''Main function used to play'''
-    choice = (int(input('Choose a row to play: ')), int(input('Choose a column to play: ')))
+    def near_bomb(self):
+        '''Function that modify the bomb board to display the number of bomb within a 1 tile range'''
+        for i in range(len(self.bomb_board)):
+            for j in range(len(self.bomb_board)):
 
-    if board[choice[0]][choice[1]] == 'X':
-        game_over()
-    else:
-        refresh_board(choice)
+                count = 0   # How many bombs near my position ?
+                if self.bomb_board[i][j] == 'X':    # If the tile is already a bomb, we can continue
+                    continue
+                for direction in DIRECTIONS:
+                    if i + direction[0] >= 0 and i + direction[0] < len(self.bomb_board) and j + direction[1] >= 0 and j + direction[1] < len(self.bomb_board):
+                        if self.bomb_board[i + direction[0]][j + direction[1]] == 'X':
+                            count += 1
+                if count > 0:
+                    self.bomb_board[i][j] = count
 
 
-def game_over():
-    '''Function use to display the game over'''
-    global GAME_OVER
+    def refresh_board(self, choice, visited = set()):
+        '''Function that refreshes the board for the user, showing blank spaces and number of bomb near the tile chosen'''
 
-    print('Sorry, you picked a bomb!')
-    GAME_OVER = True
+        if choice not in visited:   # If we didn't already go on that tile
+            if self.bomb_board[choice[0]][choice[1]] == '■':    # Check if the tile is empty or not
+                self.user_board[choice[0]][choice[1]] = ' '     # We reveal it
+                for direction in [(0,1), (1,0), (0,-1), (-1,0)]:    # Recursive call for each direction 
+                    if choice[0] + direction[0] >= 0 and choice[0] + direction[0] < len(self.user_board) and choice[1] + direction[1] >= 0 and choice[1] + direction[1] < len(self.user_board): # To avoid index out of range errors
+                        visited.add(choice)
+                        self.refresh_board((choice[0] + direction[0], choice[1] + direction[1]))
+            else:
+                self.user_board[choice[0]][choice[1]] = self.bomb_board[choice[0]][choice[1]]   # We show the number of bomb near the tile
+
+
+    def play(self, board):
+        '''Main function used to play'''
+        choice = (int(input('Choose a row to play: ')), int(input('Choose a column to play: ')))
+
+        if board[choice[0]][choice[1]] == 'X':  # If the tile chosen is a bomb, we launch the game over sequence
+            self.game_over()
+        else:   # Else, we refresh the board and continue to play 
+            self.refresh_board(choice)
+
+
+    def game_over(self):
+        '''Function use to display the game over'''
+        print('Sorry, you picked a bomb!')
+        self.game_over_value = True
+
+
+def choose_difficulty():
+    '''Function used to choose the difficulty'''
+
+    window = tk.Tk()
+    window.title("Minesweeper - Choose a difficulty")
+    for i in range(4):  # Creation of four buttons to choose difficulty
+        window.columnconfigure(i, weight=1, minsize=150)
+        window.rowconfigure(0, weight=1, minsize=75)
+
+        frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=0, column=i)
+        label = tk.Button(master=frame, text=f"{list(BOARD_INFO)[i]}", command=lambda x=f"{list(BOARD_INFO)[i]}": main(x, window))
+        label.pack()
+
+    window.mainloop()
+
+def main(difficulty, window):
+    window.destroy()
+
+    game = Minesweeper(difficulty)
+
+    game.bombify()
+    game.near_bomb()
+
+    game.display_board(game.bomb_board)
+
+    game.init_window()
+
+    while True:
+
+        if game.game_over_value:
+            break
+
+        game.display_board(game.user_board)
+        game.play(game.bomb_board)
 
 
 if __name__ == '__main__':
 
-    BOMB_BOARD = init_board(BOARD_INFO['easy'][0])
-    USER_BOARD = init_board(BOARD_INFO['easy'][0])
-
-    bombify(BOMB_BOARD, 'easy')
-    near_bomb(BOMB_BOARD)
-
-    display_board(BOMB_BOARD)
-
-    while True:
-
-        if GAME_OVER:
-            break
-
-        display_board(USER_BOARD)
-        play(BOMB_BOARD)
+    difficulty = choose_difficulty()
